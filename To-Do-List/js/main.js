e341d32f8acbc70d6fa68ef0d84b95a7bf154e90
abdullox0900 <abdullox0900@gmail.com>
2023-02-23 23:43:59 +0500

    [ADD]: 4-day --abdu

---
+++ To-Do-List/js/main.js
@@ -0,0 +1,5 @@
+document.querySelector('#to-btn').onClick = function() {
+    if(document.querySelectorAll('#to-input').value.length == 0) {
+        alert('Hello')
+    }
+}
---
+++ To-Do-List/js/script.js
@@ -0,0 +1,133 @@
+"use strict";
+
+document.body.style.backgroundColor = "#E0F0FD";
+
+let elForm = document.querySelector(".form");
+let elInput = document.querySelector(".form__input");
+let elList = document.querySelector(".list");
+let elBtnClear = document.querySelector(".btn-clear");
+
+let elAllRes = document.querySelector(".res-all");
+let elComRes = document.querySelector(".res-com");
+let elUncomRes = document.querySelector(".res-uncom");
+
+let elAllBtn = document.querySelector(".btn-all");
+let elComBtn = document.querySelector(".btn-com");
+let elUncomBtn = document.querySelector(".btn-uncom");
+
+// RENDER-TODOS
+let todoRender = function (arr, element) {
+    arr.forEach(newArr => {
+
+        let elListItem = document.createElement("li");
+        let elCloseBtn = document.createElement("button");
+        let elCheckbox = document.createElement("input");
+        let elSpan = document.createElement("item__run");
+
+        elListItem.setAttribute("class", "list__item");
+        elCloseBtn.setAttribute("class", "close__btn");
+        elCheckbox.setAttribute("class", "item__checkbox");
+        elSpan.setAttribute("class", "item__run");
+
+        elListItem.textContent = newArr.name;
+        elCloseBtn.dataset.todoId = newArr.id;
+        elCheckbox.type = "checkbox";
+        elCheckbox.dataset.checkId = newArr.id;
+
+        if (newArr.isCompleted) {
+            elCheckbox.checked = true;
+        }
+
+        element.appendChild(elListItem);
+        elListItem.appendChild(elCloseBtn);
+        elListItem.appendChild(elCheckbox);
+        elListItem.appendChild(elSpan);
+
+        elAllRes.textContent = todosArr.length;
+        elComRes.textContent = todosArr.filter((todo) => todo.isCompleted).length
+        elUncomRes.textContent = todosArr.filter((todo) => !todo.isCompleted).length
+    });
+}
+
+// LOCAL-STORAGE
+const todosLocal = JSON.parse(window.localStorage.getItem("todosLocal"))
+
+let todosArr = todosLocal || [];
+
+todoRender(todosArr, elList);
+
+// EL-LIST
+elList.addEventListener("click", function (evt) {
+    if (evt.target.matches(".close__btn")) {
+        let todoIdBtn = evt.target.dataset.todoId * 1;
+        let todoFound = todosArr.findIndex((todo) => todo.id === todoIdBtn);
+
+        todosArr.splice(todoFound, 1);
+        elList.innerHTML = null;
+
+        window.localStorage.setItem("todosLocal", JSON.stringify(todosArr));
+
+        todoRender(todosArr, elList);
+    }
+    if (evt.target.matches(".item__checkbox")) {
+        let checkboxTodoId = evt.target.dataset.checkId * 1;
+        let foundCheckTodo = todosArr.find((todo) => todo.id === checkboxTodoId);
+
+        foundCheckTodo.isCompleted = !foundCheckTodo.isCompleted;
+        elList.innerHTML = null;
+
+        window.localStorage.setItem("todosLocal", JSON.stringify(todosArr));
+
+        todoRender(todosArr, elList)
+    }
+})
+
+// EL-FORM
+elForm.addEventListener("submit", function (evt) {
+    evt.preventDefault()
+
+    let inputValue = elInput.value;
+
+    let todosObj = {
+        id: todosArr[todosArr.length - 1]?.id + 1 || 0,
+        name: inputValue,
+        isCompleted: false,
+    }
+
+    window.localStorage.setItem("todosLocal", JSON.stringify(todosArr));
+    todosArr.push(todosObj);
+
+    elList.innerHTML = null;
+    elInput.value = null;
+
+    todoRender(todosArr, elList);
+});
+
+
+elBtnClear.addEventListener("click", function () {
+    elList.innerHTML = null;
+    elAllRes.innerHTML = 0;
+    elComRes.innerHTML = 0;
+    elUncomRes.innerHTML = 0;
+    todosArr.splice(0, todosArr.length);
+});
+
+elAllBtn.addEventListener("click", function () {
+    elList.innerHTML = null
+
+    todoRender(todosArr, elList)
+});
+
+elComBtn.addEventListener("click", function () {
+    let filteredComplete = todosArr.filter((todo) => todo.isCompleted)
+    elList.innerHTML = null;
+
+    todoRender(filteredComplete, elList)
+});
+
+elUncomBtn.addEventListener("click", function () {
+    let filteredComplete = todosArr.filter((todo) => !todo.isCompleted);
+    elList.innerHTML = null;
+
+    todoRender(filteredComplete, elList)
+});
